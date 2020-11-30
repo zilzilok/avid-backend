c0556449787e39dc86479f710470f45b3af04046
Tagir <tnsafin@edu.hse.ru>
2020-11-30 23:51:28 +0300

    init gradle project

---
+++ src/main/java/ru/zilzilok/avid/AvidBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.zilzilok.avid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvidBackendApplication.class, args);
+	}
+
+}
