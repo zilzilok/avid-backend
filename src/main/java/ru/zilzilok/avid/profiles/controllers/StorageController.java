6982a3f7fd879ed2284028ea29273225d9ffa44c
Tagir <tnsafin@edu.hse.ru>
2021-04-11 16:18:35 +0300

    added update storage request (delete + upload)

--- src/main/java/ru/zilzilok/avid/profiles/controllers/StorageController.java
+++ src/main/java/ru/zilzilok/avid/profiles/controllers/StorageController.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.zilzilok.avid.profiles.models.entities.User;
 import ru.zilzilok.avid.profiles.services.StorageService;
+import ru.zilzilok.avid.profiles.services.UserService;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/user/storage")
 public class StorageController {
     private final StorageService storageService;
+    private final UserService userService;
 
     @Autowired
-    StorageController(StorageService storageService) {
+    StorageController(StorageService storageService, UserService userService) {
         this.storageService = storageService;
+        this.userService = userService;
     }
 
     @PostMapping("/upload")
@@ -24,6 +30,18 @@ public class StorageController {
             return ResponseEntity.ok(storageService.uploadFile(file));
         }
         return ResponseEntity.badRequest().body("File can't be upload.");
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestPart(value = "file") MultipartFile file, Principal p) {
+        User user = userService.findByUsername(p.getName());
+        if (ObjectUtils.isNotEmpty(file)) {
+            if(StringUtils.isNotBlank(user.getPhotoPath())) {
+                storageService.deleteFileFromS3Bucket(user.getPhotoPath());
+            }
+            return ResponseEntity.ok(storageService.uploadFile(file));
+        }
+        return ResponseEntity.badRequest().body("File can't be update.");
     }
 
     @DeleteMapping("/delete")
