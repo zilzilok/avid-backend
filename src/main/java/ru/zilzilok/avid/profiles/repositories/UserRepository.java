82d32accd0702a67b4fd00023d23ab75dee386b6
Tagir <tnsafin@edu.hse.ru>
2021-04-12 23:22:23 +0300

    added username for get request query param "startsWith"

--- src/main/java/ru/zilzilok/avid/profiles/repositories/UserRepository.java
+++ src/main/java/ru/zilzilok/avid/profiles/repositories/UserRepository.java
@@ -16,9 +16,21 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByActivationCode(String code);
 
-    @Query("SELECT DISTINCT u FROM User u WHERE LOWER(u.firstName) LIKE LOWER(CONCAT(:startWith, '%')) OR LOWER(u.secondName) LIKE LOWER(CONCAT(:startWith, '%'))")
-    List<User> findByNameStartingWithIgnoreCase(String startWith, Pageable pageable);
+    @Query("SELECT DISTINCT u FROM User u " +
+            "WHERE " +
+            "LOWER(u.firstName) LIKE LOWER(CONCAT(:startsWith, '%')) " +
+            "OR LOWER(u.secondName) LIKE LOWER(CONCAT(:startsWith, '%')) " +
+            "OR LOWER(u.username) LIKE LOWER(CONCAT(:startsWith, '%'))")
+    List<User> findByNameStartingWithIgnoreCase(String startsWith, Pageable pageable);
 
-    @Query("SELECT DISTINCT f FROM User u JOIN u.friends f WHERE (u.username = :username AND (LOWER(f.firstName) LIKE LOWER(CONCAT(:startWith, '%')) OR LOWER(f.secondName) LIKE LOWER(CONCAT(:startWith, '%')))) ORDER BY f.secondName")
-    List<User> findFriendsByNameStartingWithIgnoreCase(String username, String startWith);
+    @Query("SELECT DISTINCT f FROM User u " +
+            "JOIN u.friends f " +
+            "WHERE " +
+            "(u.username = :username " +
+            "AND (LOWER(f.firstName) LIKE LOWER(CONCAT(:startsWith, '%')) " +
+            "OR LOWER(f.secondName) LIKE LOWER(CONCAT(:startsWith, '%')) " +
+            "OR LOWER(f.username) LIKE LOWER(CONCAT(:startsWith, '%')))" +
+            ") " +
+            "ORDER BY f.secondName")
+    List<User> findFriendsByNameStartingWithIgnoreCase(String username, String startsWith);
 }
